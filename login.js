dffd4eea5fd0680815c32f933e952536c0afa648
leandromribeiro <leandromribeiro@gmail.com>
2024-05-01 16:28:46 -0300

    Nova home finalizada

--- login.js
+++ login.js
@@ -2,3 +2,10 @@
 //Trabalhando no login
 
 // Finalizando sistema de login
+function login(nome, email){
+    return 'Bem vindo';
+}
+
+
+// Seja bem vindo
+// Retornamos o nome do usuaario quando logar
